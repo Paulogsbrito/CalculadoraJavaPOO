f25f69c609c4bf1829fcaeb625003c16b7889221
Paulo Gustavo Brito <paulogsbrito@gmail.com>
2025-09-13 21:13:40 -0300

    Update CalculadoraJavaTest.java
    Adicionado mais Testes Unitarios de Operações de Calculo

--- src/CalculadoraJavaTest.java
+++ src/CalculadoraJavaTest.java
@@ -28,8 +28,75 @@ public class CalculadoraJavaTest {
         List<String> historico = calculadora.getHistorico();
 
         assertEquals(1, historico.size(), "O historico deveria ter 1 operação");
-        assertTrue(historico.contains("128.0 + 8.0 = 128"));
+        assertTrue(historico.contains("128.0 + 8.0 = 136"));
 
     }
+
+    @Test
+    void testSubtrair(){
+        double resultado = calculadora.subtrair(128, 8);
+        assertEquals(120,resultado,0.0001, "128 - 8 deveria ser 120");
+
+    }
+
+    @Test
+    void testHistoricoSubtrair(){
+        calculadora.subtrair(128,8);
+        List<String> historico = calculadora.getHistorico();
+
+        assertEquals(1, historico.size(),"O historico deveria ter 1 operação");
+        assertTrue(historico.contains("128.0 - 8.0 = 120"));
+
+
+    }
+
+    @Test
+    void testMultiplicar(){
+        double resultado = calculadora.multiplicar(128,2);
+        assertEquals(256,resultado,0.0001, "128 * 2 deveria ser 256");
+
+    }
+
+
+
+    @Test
+    void testHistoricoMultiplicar(){
+
+        calculadora.multiplicar(128,2);
+        List<String> historico = calculadora.getHistorico();
+
+        assertEquals(1, historico.size(), "O historico deveria ter 1 operação");
+        assertTrue(historico.contains("128 * 2 = 256"));
+
+    }
+
+    @Test
+    void testDividir(){
+        double resultado = calculadora.dividir(128,2);
+        assertEquals(64,resultado,0.0001,"128 / 2 deveria ser 64");
+
+    }
+
+    @Test
+    void testHistoricoDividir(){
+
+        calculadora.dividir(128,2);
+        List<String> historico = calculadora.getHistorico();
+
+        assertEquals(1, historico.size(), "O historico deveria ter 1 operação");
+        assertTrue(historico.contains("128 / 2 = 64"));
+    }
+
+    @Test
+    void testDivisaoPorZero(){
+        double resultado = calculadora.dividir(10,0);
+        assertTrue(Double.isNaN(resultado), "Divisão por zero deve retornar NaN!!!");
+
+        List<String> historico = calculadora.getHistorico();
+        assertEquals(1, historico.size(),"O historico deve registrar erro de Divisão por Zero");
+        assertTrue(historico.get(0).contains("Erro de Divisão por Zero!!!"));
+    }
+
+
 
 }
